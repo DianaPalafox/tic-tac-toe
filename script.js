07e287c9b978f933bb3ade0063d596182bdf1fde
Diana Palafox <dianapalafox95@gmail.com>
2022-07-18 18:15:51 -0500

    Create a function  that checks for when the game is over and announces the winner

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+(() => { 
+const arr = ['', '', '', '', '', '', '', '', '']
 const Form = (() => {
     const btnNewGame = document.querySelector('#start-game')
     btnNewGame.addEventListener('click', () => {
@@ -8,38 +11,49 @@ const Form = (() => {
     closeSpan.addEventListener('click', () => {
         document.querySelector('#form').style.display = 'none';
     });
- })();
-  
 
-  
- const submit = (() => {
     const submitBtn = document.querySelector('#submit')
     submitBtn.addEventListener('click', () => {
         document.querySelector('#form').style.display = 'none';
         document.querySelector('#start-game').style.display = 'none'
-        playersTurnOne();
+        players.playersTurnOne();
     });
-   
+ })();
+  
+  
+ const players = (() => {
+    const playersContainer = document.querySelector('.players-container')
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('info-container');
+
     function playersTurnOne() {
-        const playersContainer = document.querySelector('.players-container')
-        const infoContainer = document.createElement('div');
-        infoContainer.classList.add('info-container');
-       
         const playerOne = Player(document.querySelector('.player-one').value);
         infoContainer.textContent = playerOne.getName();
   
         playersContainer.appendChild(infoContainer);   
     }
 
-    function playersTurnTwo() {
-        const playersContainer = document.querySelector('.players-container')
-        const infoContainer = document.createElement('div');
-        infoContainer.classList.add('info-container');
-       
+    function playersTurnTwo() {   
         const playerTwo = Player(document.querySelector('.player-two').value);
         infoContainer.textContent = playerTwo.getName();
   
         playersContainer.appendChild(infoContainer);   
+    }
+
+    function winnerOne() {
+        const playerOne = Player(document.querySelector('.player-one').value);
+        infoContainer.textContent = playerOne.winner();
+  
+        playersContainer.appendChild(infoContainer);   
+
+    }
+
+    function winnerTwo() {
+        const playerTwo = Player(document.querySelector('.player-two').value);
+        infoContainer.textContent = playerTwo.winner();
+  
+        playersContainer.appendChild(infoContainer);   
+
     }
     
     function deletePlayersTurn() {
@@ -49,54 +63,101 @@ const Form = (() => {
     }
 
     return {
-        playersTurnOne, playersTurnTwo, deletePlayersTurn
+        playersTurnOne, playersTurnTwo, winnerOne, winnerTwo, deletePlayersTurn
     }
 
  })();
   
-  
- const buildGameBoard = (() => {
-    let gameboardArray = [];
+
+ const GameBoard = (() => {
     const submitBtn = document.querySelector('#submit')
+    const boardgameContainer = document.querySelector('.boardgame')
     submitBtn.addEventListener('click', () => {
-        for (i=0; i<9; i++) {
-        const boardgameContainer = document.querySelector('.boardgame')
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.setAttribute("data-index", `${i}`);
-        content.style.border = "thin solid black";
-       
-        //const boxContainer = document.createElement('div');
-        //boxContainer.classList.add('box-container');
-        
-        //content.appendChild(boxContainer);
-        boardgameContainer.appendChild(content);
-    }
+        renderBoard(); 
     });  
-  
+
+    function renderBoard() {
+        for (i=0; i<9; i++) {
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.style.border = "thin solid black";
+            content.setAttribute('data-index', `${i}`)
+            //const boxContainer = document.createElement('div');
+            //boxContainer.classList.add('box-container');
+            
+            //content.appendChild(boxContainer);
+            boardgameContainer.appendChild(content);
+        }
+
+    }
+
  })();
  
 
 const appendToDOM = (() => {
     let turn = 0;
-    document.addEventListener('click', function(e) {    
+    document.addEventListener('click', function(e) {  
+        let i = e.target.dataset.index  
         if (e.target.classList.contains('content') && e.target.textContent === '') {
-            submit.deletePlayersTurn(); 
+            players.deletePlayersTurn(); 
             turn++; 
             if(turn % 2 === 0 ) {
                 e.target.textContent = 'O'; 
-                submit.playersTurnOne();
+                players.playersTurnOne();  
+                arr[`${i}`] = 'O'
+                console.log(arr)
+                checkForWin();            
             }
             else {
                 e.target.textContent = 'X'; 
-                submit.playersTurnTwo();
+                players.playersTurnTwo();
+                arr[`${i}`] = 'X'
+                console.log(arr)
+                checkForWin(); 
+                
             }
         }
+
     })
+
+    function checkForWin() {
+        if(arr[0] == 'X' && arr[1] == 'X' && arr[2] == 'X' || 
+        arr[3] == 'X' && arr[4] == 'X' && arr[5] == 'X' || 
+        arr[6] == 'X' && arr[7] == 'X' && arr[8] == 'X') {
+            return players.winnerOne();
+        }
+        if(arr[0] == 'O' && arr[1] == 'O' && arr[2] == 'O' || 
+        arr[3] == 'O' && arr[4] == 'O' && arr[5] == 'O' || 
+        arr[6] == 'O' && arr[7] == 'O' && arr[8] == 'O') {
+            return players.winnerTwo();
+        }
+        if(arr[0] == 'X' && arr[3] == 'X' && arr[6] == 'X' || 
+        arr[1] == 'X' && arr[4] == 'X' && arr[7] == 'X' || 
+        arr[2] == 'X' && arr[5] == 'X' && arr[8] == 'X') {
+            return players.winnerOne();
+        }
+        if(arr[0] == 'O' && arr[3] == 'O' && arr[6] == 'O' || 
+        arr[1] == 'O' && arr[4] == 'O' && arr[7] == 'O' || 
+        arr[2] == 'O' && arr[5] == 'O' && arr[8] == 'O') {
+            return players.winnerTwo();
+        }
+        if(arr[0] == 'X' && arr[4] == 'X' && arr[8] == 'X' || 
+        arr[2] == 'X' && arr[4] == 'X' && arr[6] == 'X') {
+            return players.winnerOne();
+        }
+        if(arr[0] == 'O' && arr[4] == 'O' && arr[8] == 'O' || 
+        arr[2] == 'O' && arr[4] == 'O' && arr[6] == 'O') {
+            return players.winnerTwo();
+        }
+
+    }     
     
 })(); 
-  
+
 const Player = (name) => {
     const getName = () => `${name}` + "'s turn";
-    return { getName }; 
+    const winner = () => `${name}` + "'s the winner!";
+    return { getName, winner }; 
 };
+
+})(); 

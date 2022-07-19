11576161b0a415f05de070cacd193270b68a7f1b
Diana Palafox <dianapalafox95@gmail.com>
2022-07-19 12:30:19 -0500

    Add a button that appears at the end of the game and allows you to start a new one

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 (() => { 
-const arr = ['', '', '', '', '', '', '', '', '']
+let arr = ['', '', '', '', '', '', '', '', '']
+let turn = 0; 
+let gameOver = false; 
 const Form = (() => {
     const btnNewGame = document.querySelector('#start-game')
     btnNewGame.addEventListener('click', () => {
@@ -30,7 +32,7 @@ const Form = (() => {
         const playerOne = Player(document.querySelector('.player-one').value);
         infoContainer.textContent = playerOne.getName();
   
-        playersContainer.appendChild(infoContainer);   
+        playersContainer.appendChild(infoContainer);  
     }
 
     function playersTurnTwo() {   
@@ -44,7 +46,7 @@ const Form = (() => {
         const playerOne = Player(document.querySelector('.player-one').value);
         infoContainer.textContent = playerOne.winner();
   
-        playersContainer.appendChild(infoContainer);   
+        playersContainer.appendChild(infoContainer);  
 
     }
 
@@ -53,7 +55,11 @@ const Form = (() => {
         infoContainer.textContent = playerTwo.winner();
   
         playersContainer.appendChild(infoContainer);   
+    }
 
+    function tie() {
+        infoContainer.textContent = "It's a tie";
+        playersContainer.appendChild(infoContainer);   
     }
     
     function deletePlayersTurn() {
@@ -63,7 +69,7 @@ const Form = (() => {
     }
 
     return {
-        playersTurnOne, playersTurnTwo, winnerOne, winnerTwo, deletePlayersTurn
+        playersTurnOne, playersTurnTwo, winnerOne, winnerTwo, tie, deletePlayersTurn
     }
 
  })();
@@ -88,17 +94,28 @@ const Form = (() => {
             //content.appendChild(boxContainer);
             boardgameContainer.appendChild(content);
         }
-
     }
-
+    
+    function deleteBoard() {
+        arr = ['', '', '', '', '', '', '', '', ''];
+        turn = 0;
+        const content = document.querySelector('.content')
+        if(typeof(content) != 'undefined' && content != null) {     
+            for (i=0; i<9; i++) {
+                const content = document.querySelector('.content')
+                boardgameContainer.removeChild(content); 
+            }
+        return gameOver = false;   
+    }
+    }
+    return { renderBoard, deleteBoard }
  })();
  
 
 const appendToDOM = (() => {
-    let turn = 0;
-    document.addEventListener('click', function(e) {  
+    document.addEventListener('click', function(e) { 
         let i = e.target.dataset.index  
-        if (e.target.classList.contains('content') && e.target.textContent === '') {
+        if (e.target.classList.contains('content') && e.target.textContent === '' && gameOver === false) {
             players.deletePlayersTurn(); 
             turn++; 
             if(turn % 2 === 0 ) {
@@ -124,39 +141,61 @@ const appendToDOM = (() => {
         if(arr[0] == 'X' && arr[1] == 'X' && arr[2] == 'X' || 
         arr[3] == 'X' && arr[4] == 'X' && arr[5] == 'X' || 
         arr[6] == 'X' && arr[7] == 'X' && arr[8] == 'X') {
+            gameOver = true; 
+            displayController();
             return players.winnerOne();
         }
         if(arr[0] == 'O' && arr[1] == 'O' && arr[2] == 'O' || 
         arr[3] == 'O' && arr[4] == 'O' && arr[5] == 'O' || 
         arr[6] == 'O' && arr[7] == 'O' && arr[8] == 'O') {
+            gameOver = true; 
+            displayController();
             return players.winnerTwo();
         }
         if(arr[0] == 'X' && arr[3] == 'X' && arr[6] == 'X' || 
         arr[1] == 'X' && arr[4] == 'X' && arr[7] == 'X' || 
         arr[2] == 'X' && arr[5] == 'X' && arr[8] == 'X') {
+            gameOver = true; 
+            displayController();
             return players.winnerOne();
         }
         if(arr[0] == 'O' && arr[3] == 'O' && arr[6] == 'O' || 
         arr[1] == 'O' && arr[4] == 'O' && arr[7] == 'O' || 
         arr[2] == 'O' && arr[5] == 'O' && arr[8] == 'O') {
+            gameOver = true; 
+            displayController();
             return players.winnerTwo();
         }
         if(arr[0] == 'X' && arr[4] == 'X' && arr[8] == 'X' || 
         arr[2] == 'X' && arr[4] == 'X' && arr[6] == 'X') {
+            gameOver = true; 
+            displayController();
             return players.winnerOne();
         }
         if(arr[0] == 'O' && arr[4] == 'O' && arr[8] == 'O' || 
         arr[2] == 'O' && arr[4] == 'O' && arr[6] == 'O') {
+            gameOver = true; 
+            displayController();
             return players.winnerTwo();
         }
 
-    }     
+    }  
+    
+    function displayController() {
+        const newGameBtn = document.querySelector('#new-game')
+        newGameBtn.style.display = 'block'; 
+        newGameBtn.addEventListener('click', () => {
+            newGameBtn.style.display = 'none'; 
+            document.querySelector('#form').style.display ='block';
+            GameBoard.deleteBoard();      
+        })
+    }
     
 })(); 
 
 const Player = (name) => {
     const getName = () => `${name}` + "'s turn";
-    const winner = () => `${name}` + "'s the winner!";
+    const winner = () => `${name}` + " is the winner!";
     return { getName, winner }; 
 };
 
